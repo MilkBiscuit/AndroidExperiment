3402531cb36cfb1e8974235a6623588c99a99d29
Chandler Cheng <dc9909@hotmail.com>
2022-03-09 10:55:59 +1300

    Change back MainActivity as the launcher activity

--- app/src/main/java/com/cheng/experimentapp/ui/InputModeDemoScreen.kt
+++ app/src/main/java/com/cheng/experimentapp/ui/InputModeDemoScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,22 +28,24 @@ fun InputModeDemoScreen() {
 
 @Composable
 fun InputModeDemoView() {
-    Column(
-        Modifier.fillMaxSize(),
-    ) {
-        Box(
-            modifier = Modifier.weight(1f).fillMaxWidth().background(color = Color.Red)
-        )
-        Box(
-            modifier = Modifier.weight(1f).fillMaxWidth().background(color = Color.Yellow)
-        )
-        Box(
-            modifier = Modifier.weight(1f).fillMaxWidth().background(color = Color.Green)
-        )
+    Box {
+        Column(
+            Modifier.fillMaxSize(),
+        ) {
+            Box(
+                modifier = Modifier.weight(1f).fillMaxWidth().background(color = Color.Red)
+            )
+            Box(
+                modifier = Modifier.weight(1f).fillMaxWidth().background(color = Color.Yellow)
+            )
+            Box(
+                modifier = Modifier.weight(1f).fillMaxWidth().background(color = Color.Green)
+            )
+        }
         TextField(
             value = "no need to input",
             onValueChange = {},
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth().align(Alignment.TopCenter)
         )
     }
 }
