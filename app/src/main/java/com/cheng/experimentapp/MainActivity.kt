503031ecf391d509ca7edff981988c7009c68671
Chandler Cheng <dc9909@hotmail.com>
2022-04-06 11:49:39 +1200

    Try update UI while it's in background

--- app/src/main/java/com/cheng/experimentapp/MainActivity.kt
+++ app/src/main/java/com/cheng/experimentapp/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.cheng.experimentapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.View
-import com.cheng.experimentapp.usecase.LaunchAppUC
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.btn_restart).setOnClickListener {
-            val launchAppUC = LaunchAppUC(this)
-            launchAppUC.immediateRestart()
+        findViewById<View>(R.id.button).setOnClickListener {
+            Handler(Looper.getMainLooper()).postDelayed({
+                Log.e("trpb67", "button instance:" + findViewById<Button>(R.id.button))
+                findViewById<Button>(R.id.button).text = "Hello World"
+            }, 5000)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("trpb67", "onDestroy")
     }
 }
