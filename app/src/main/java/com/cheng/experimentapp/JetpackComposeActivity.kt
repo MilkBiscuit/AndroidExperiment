e4bca8cb715b562a15194ec46a59a647a533e251
Chandler Cheng <dc9909@hotmail.com>
2024-08-22 14:36:35 +1200

    Upgrade gradle and lib versions

--- app/src/main/java/com/cheng/experimentapp/JetpackComposeActivity.kt
+++ app/src/main/java/com/cheng/experimentapp/JetpackComposeActivity.kt
@@ -3,7 +3,7 @@ package com.cheng.experimentapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.cheng.experimentapp.ui.AnimatedVectorDrawableDemoScreen
+import com.cheng.experimentapp.ui.InputModeDemoScreen
 import com.cheng.experimentapp.ui.theme.ExperimentAppTheme
 
 class JetpackComposeActivity : ComponentActivity() {
@@ -12,7 +12,7 @@ class JetpackComposeActivity : ComponentActivity() {
         setContent {
             ExperimentAppTheme {
                 // A surface container using the 'background' color from the theme
-                AnimatedVectorDrawableDemoScreen()
+                InputModeDemoScreen()
             }
         }
     }
