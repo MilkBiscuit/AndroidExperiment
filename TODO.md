7fe413516f8555a0535f7734592d481702fe1816
Chandler Cheng <dc9909@hotmail.com>
2024-08-27 10:17:34 +1200

    Change InputModeDemoScreen

---
+++ TODO.md
@@ -0,0 +1,3 @@
+
+## High Priority
+- Migrate gradle build configuration to kotlin
--- app/src/main/java/com/cheng/experimentapp/ui/InputModeDemoScreen.kt
+++ app/src/main/java/com/cheng/experimentapp/ui/InputModeDemoScreen.kt
@@ -1,16 +1,20 @@
 package com.cheng.experimentapp.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.cheng.experimentapp.ui.theme.ExperimentAppTheme
 
 @Composable
@@ -21,32 +25,26 @@ fun InputModeDemoScreen() {
                 Text("windowSoftInputMode demo")
             }
         }
-    ) {
-        InputModeDemoView()
+    ) { padding ->
+        InputModeDemoView(modifier = Modifier.padding(padding))
     }
 }
 
 @Composable
-private fun InputModeDemoView() {
-    Box {
+private fun InputModeDemoView(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier.fillMaxSize()) {
         Column(
-            Modifier.fillMaxSize(),
+            modifier.align(Alignment.Center),
         ) {
-            Box(
-                modifier = Modifier.weight(1f).fillMaxWidth().background(color = Color.Red)
-            )
-            Box(
-                modifier = Modifier.weight(1f).fillMaxWidth().background(color = Color.Yellow)
-            )
-            Box(
-                modifier = Modifier.weight(1f).fillMaxWidth().background(color = Color.Green)
+            Text("Hello world")
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = "no need to input anything",
+                onValueChange = {},
             )
         }
-        TextField(
-            value = "no need to input anything",
-            onValueChange = {},
-            modifier = Modifier.fillMaxWidth().align(Alignment.TopCenter)
-        )
     }
 }
 
